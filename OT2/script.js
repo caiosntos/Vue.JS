5da0a8208fa34205c973e207c80d12e0530f2494
caiosntos <caiogiovanists@outlook.com>
2025-05-14 20:07:33 -0300

    feat: Trilhas da OT2

---
+++ OT2/script.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        url:'https://vuejs.org'
+    }
+})
+
+var app2 = new Vue({
+    el:'#app2',
+    data:{
+        message:''
+    }
+})
+
+var app3 = new Vue({
+    el:'#app3',
+    data:{
+        seen:true
+    }
+})
+
+var app4 = new Vue({
+    el:'#app4',
+    data:{
+        usuario:{
+            id:1,
+            nome:'Caio',
+            email:'caio@caio.com',
+            profissao:'Programador'
+        }
+    }
+})
