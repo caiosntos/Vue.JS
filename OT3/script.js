eb1ee8453831b1ffc76c7192b16d60283ee1f718
caiosntos <caiogiovanists@outlook.com>
2025-05-15 19:15:06 -0300

    Iniciando OT3

---
+++ OT3/script.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        titulo:'Apredendo Vue JS',
+        nome: '',
+        telefone:'',
+        novidades:'',
+        interesses:[],
+        conheceu:''
+    }
+})
